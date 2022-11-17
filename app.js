1c513938174864926f3c5fc43df7f384b05d33f4
Joshua Ombasa <joshuaombasa@gmail.com>
2022-11-17 14:50:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+let count = 0;
+const value = document.querySelector('#value')
+const increase = document.querySelector('.increase')
+const reset = document.querySelector('.reset')
+const decrease = document.querySelector('.decrease')
+
+increase.addEventListener('click', e => {
+    const actions = e.currentTarget.classList;
+    if(actions.contains('increase')) {
+        count++
+    }
+    value.textContent = count
+})
+
+decrease.addEventListener('click', e => {
+    const actions = e.currentTarget.classList;
+    if(actions.contains('decrease')) {
+        count--
+    }
+    value.textContent = count
+})
+
+reset.addEventListener('click', e => {
+    const actions = e.currentTarget.classList;
+    if(actions.contains('reset')) {
+        count = 0
+    }
+    value.textContent = count
+})

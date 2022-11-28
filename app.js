499837b75e2d0d3a7854e1a89522f95d9626d559
joshuaombasa <114947718+joshuaombasa@users.noreply.github.com>
2022-11-28 15:30:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// variable declarations
 let count = 0;
 const value = document.querySelector('#value')
 const increase = document.querySelector('.increase')
 const reset = document.querySelector('.reset')
 const decrease = document.querySelector('.decrease')
 
+// DOM manipulation
 increase.addEventListener('click', e => {
     const actions = e.currentTarget.classList;
     if(actions.contains('increase')) {
